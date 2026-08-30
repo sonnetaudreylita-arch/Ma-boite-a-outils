52f96e6ee8ed86be680f79c68bbddef6ca186c92
sonnetaudreylita-arch <sonnetaudreylita@gmail.com>
2026-08-30 20:36:51 +0200

    app.js

--- app.js
+++ app.js
@@ -288,4 +288,10 @@ async function load(){
   window.addEventListener('hashchange',router);
 }
 
-load();
+load(load().catch(e => {
+  document.body.innerHTML =
+    '<pre style="white-space:pre-wrap;padding:20px;color:red;font-size:16px">' +
+    'ERREUR APP.JS\n\n' +
+    e.stack +
+    '</pre>';
+});
